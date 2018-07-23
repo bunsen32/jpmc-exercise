fc1e959dbb5a78d04ec71bf7119d6e7770d4f026
Andrew Forrest <andrew@dysphoria.net>
2018-07-23 11:30:06 +0100

    Reorganises tests slightly.

--- ex1-test/src/jpinterview/ex1/MessageProcessorAndLoggerTest.java
+++ ex1-test/src/jpinterview/ex1/MessageProcessorAndLoggerTest.java
@@ -3,8 +3,19 @@ package jpinterview.ex1;
 import jpinterview.ex1.messagemodel.Message;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
+/**
+ * Very basic tests.
+ */
 public class MessageProcessorAndLoggerTest {
 
     @Test
@@ -15,5 +26,31 @@ public class MessageProcessorAndLoggerTest {
         }
 
         assertFalse(p.accept(Message.sellOne("thing", 10)));
+    }
+
+
+    @Test
+    public void processesMessagesAndLogsCorrectMessages() throws IOException {
+        try (
+            InputStream in = getClass().getResourceAsStream("test-input.txt");
+            InputStream expectedOutput = getClass().getResourceAsStream("test-output.txt")) {
+
+            BufferedReader readerIn = new BufferedReader(new InputStreamReader(in));
+            ArrayList actualOut = new ArrayList(51);
+            Logger logOut = logLine -> actualOut.add(logLine);
+            MessageProcessorAndLogger processor = new MessageProcessorAndLogger(logOut);
+            StreamProcessor.process(readerIn, processor);
+            assertEquals(fileAsStrings(expectedOutput), actualOut);
+        }
+    }
+
+    private static List<String> fileAsStrings(InputStream file) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(file));
+        List<String> result = new ArrayList<>();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            result.add(line);
+        }
+        return result;
     }
 }
